14cf09f409c463be5050182a2ec05ede12d901e6
mtarjan <max.tarjan@gmail.com>
2020-07-21 01:44:27 +0000

    save worksheets

---
+++ worksheet-3.sh
@@ -0,0 +1,12 @@
+# configure git
+
+git config --global user.name mtarjan
+git config --global user.email max.tarjan@gmail.com
+
+
+# Link your local repository to the origin repository on GitHub, by
+# copying the code shown on your GitHub repo under the heading:
+# "…or push an existing repository from the command line"
+
+git remote add origin https://github.com/mtarjan/handouts.git
+git push -u origin master
